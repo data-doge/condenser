69747e007f803a09d48df318ae6c3a7cfd5e1e76
euglazer <eugenestapler@gmail.com>
2015-12-12 22:22:29 -0800

    can display diamond

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
 $(document).ready(function () {
-  alert('wsup its jquery')
+
+  String.prototype.repeat = function (num) {
+    return new Array(num + 1).join(this)
+  }
+
+  var cell = '+ '
+  var $diamond = $('.diamond')
+  var rowLengths = [1,3,5,7,9,11,13,11,9,7,5,3,1]
+
+  _.each(rowLengths, function (rowLength) {
+    var $row = $('<div></div>').text(cell.repeat(rowLength))
+    $diamond.append($row)
+  })
+
 })
