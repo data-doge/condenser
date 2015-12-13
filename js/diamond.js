4a4bfbab767bffb6c996630c919a9252bdcb9442
euglazer <eugenestapler@gmail.com>
2015-12-13 12:00:53 -0800

    can construct rows of characters based on the rowWidths calculated from diamond's text length

--- js/diamond.js
+++ js/diamond.js
@@ -1,30 +1,47 @@
 function Diamond (text) {
   this.text = text
+  this.size = this.getSize()
+  this.rows = new Array(this.size)
   this.$el = $('.diamond')
 }
 
 Diamond.prototype.construct = function () {
-  var self = this;
+  var self = this
   _.each(this.rowWidths(), function (rowWidth) {
     var cell = '+'
     var $row = $('<div></div>').text(cell.repeat(rowWidth))
     self.$el.append($row)
   })
-};
+}
+
+Diamond.prototype.centerRowIndex = function () {
+  return Math.floor(this.size / 2)
+}
+
+Diamond.prototype.constructRows = function () {
+  var rowWidths = this.rowWidths()
+  var r = this.centerRowIndex()
+  for (var i = 0; i < this.size; i++) {
+    r === this.size - 1 ? r = 0 : r++
+    var rowWidth = rowWidths[r]
+    var rowChars = this.text.slice(0, rowWidth)
+    this.text = this.text.slice(rowWidth)
+    this.rows[r] = rowChars
+  }
+}
 
 Diamond.prototype.rowWidths = function () {
-  var centerRowWidth = this.centerRowWidth()
-  var rowWidths = [centerRowWidth]
-  for (var i = centerRowWidth - 2; i > 0; i -= 2) {
+  var rowWidths = [this.size]
+  for (var i = this.size - 2; i > 0; i -= 2) {
     rowWidths.push(i)
     rowWidths.unshift(i)
   }
   return rowWidths
 }
 
-Diamond.prototype.centerRowWidth = function () {
-  var width = Math.sqrt(2 * this.text.length)
-  width =  Math.floor(width)
-  if (width.isEven) { width++ }
-  return width
+Diamond.prototype.getSize = function () {
+  var size = Math.sqrt(2 * this.text.length)
+  size =  Math.floor(size)
+  if (size.isEven) { size++ }
+  return size
 }
