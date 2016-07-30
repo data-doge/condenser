8181d143d7c3c00cc579479d62b4b959bad7f28c
data-doge <eugene@enspiral.com>
2016-07-30 18:01:13 -0400

    localenvify google sheets credentials

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 import $ from 'jquery'
 import Diamond from './diamond.js'
+import dotenv from 'dotenv'
+dotenv.config()
 
+var creds = JSON.parse(process.env.CREDENTIALS)
+console.log('creds: ', creds)
 // ~1900 chars, 279 words
 var text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tincidunt blandit elit sit amet lobortis. Phasellus gravida id nisl eu malesuada. Morbi rutrum, ligula sit amet sodales semper, diam arcu lacinia ante, ultrices porttitor neque augue a massa. Morbi euismod arcu in tortor efficitur, nec mollis diam mollis. Aenean nec diam aliquet, rhoncus odio a, egestas est. Etiam condimentum dapibus tortor eget pellentesque. Ut ullamcorper augue vitae accumsan mattis. Vestibulum ut nibh vitae dolor condimentum sagittis. Quisque dui nisl, efficitur vel commodo a, tincidunt eget ante. Nullam iaculis vitae risus nec ullamcorper. Donec sed porta magna, cursus volutpat metus. Nam elementum sapien eget leo gravida sollicitudin. Maecenas aliquam eleifend orci. Sed lobortis augue vel justo laoreet, volutpat auctor orci luctus. Maecenas neque augue, mollis vel risus quis, cursus tempor neque. Donec nunc augue, vulputate at metus non, congue sagittis felis. In mattis viverra ipsum eget finibus. Se eleifend oid. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tincidunt blandit elit sit amet lobortis. Phasellus gravida id nisl eu malesuada. Morbi rutrum, ligula sit amet sodales semper, diam arcu lacinia ante, ultrices porttitor neque augue a massa. Morbi euismod arcu in tortor efficitur, nec mollis diam mollis. Aenean nec diam aliquet, rhoncus odio a, egestas est. Etiam condimentum dapibus tortor eget pellentesque. Ut ullamcorper augue vitae accumsan mattis. Vestibulum ut nibh vitae dolor condimentum sagittis. Quisque dui nisl, efficitur vel commodo a, tincidunt eget ante. Nullam iaculis vitae risus nec ullamcorper. Donec sed porta magna, cursus volutpat metus. Nam elementum sapien eget leo gravida sollicitudin. Maecenas aliquam eleifend orci. Sed lobortis augue vel justo laoreet, volutpat auctor orci luctus. Maecenas neque augue, mollis vel risus quis, cursus tempor neque.'
 
