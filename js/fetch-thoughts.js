582cc45701f7ee266fc9ffc7f2b27cdbd08b013f
data-doge <eugene@enspiral.com>
2016-07-31 20:01:20 -0400

    rendering bads to page

--- js/fetch-thoughts.js
+++ js/fetch-thoughts.js
@@ -3,15 +3,12 @@ import request from 'request'
 const fetchThoughts = (cb) => {
   let url = 'https://spreadsheets.google.com/feeds/list/1rZ2aDGeMN-oaRRCFHs0Ddqvgs60pIYBAAmhxuXiMtjk/od6/public/basic?alt=json'
   request(url, function (err, res, body) {
-    let rows = JSON.parse(body).feed.entry.map((row) => {
-      return {
-        type: row.title.$t,
-        text: row.content.$t.substring(6)
-       }
-    })
+    let goods = [], bads = []
 
-    let goods = rows.filter((row) => row.type === 'positive')
-    let bads = rows.filter((row) => row.type === 'negative')
+    let rows = JSON.parse(body).feed.entry.forEach((row) => {
+      let type = row.title.$t, text = row.content.$t.substring(6)
+      type === 'positive' ? goods.push(text) : bads.push(text)
+    })
 
     cb(goods, bads)
   })
